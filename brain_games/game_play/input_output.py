03f0ec81e6be6d1c8838fd5e04e3642f0680f7a1
Дмитрий Губин <dgubin71@gmail.com>
2022-06-22 18:39:03 +0300

    Add module input_output

---
+++ brain_games/game_play/input_output.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import prompt
+
+
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    return name
+
+
+def chek_answer(question, true_answer, name):
+    print(f'Question: {question}')
+    answer = prompt.string('Your answer: ')
+    if answer == true_answer:
+        print('Correct !')
+        return True
+    else:
+        print(f"'{answer}' is wrong answer ;(."
+              f" Correct answer was  '{true_answer}'.")
+        print(f"Let's try again, {name}!")
+        return False
