6d0c1a9dc1fdb096127331ac7ada6c9f98f4954e
Дмитрий Губин <dgubin71@gmail.com>
2022-06-22 18:37:29 +0300

    modify game_even.py. it use module input_output

--- brain_games/game_play/game_even.py
+++ brain_games/game_play/game_even.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python3
-import prompt
 from random import randrange
+from brain_games.game_play.input_output import welcome_user
+from brain_games.game_play.input_output import chek_answer
 
 
 def even_not_even():
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    name = welcome_user()
 
     answer_ok = 0               # counter correct answer
     while answer_ok != 3:
         number = randrange(100)
         true_answer = 'yes' if number % 2 == 0 else 'no'
-        print(f'Question: {number}')
-        answer = prompt.string('Your answer: ')
-        if answer == true_answer:
+        if chek_answer(number, true_answer, name):
             answer_ok += 1
-            print('Correct !')
         else:
-            print(f"'{answer}' is wrong answer ;(."
-                  f" Correct answer was  '{true_answer}'.")
-            print(f"Let's try again, {name}!")
             break
     else:
         print(f'Congratulations, {name}!')
