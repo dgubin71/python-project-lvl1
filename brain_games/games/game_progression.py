ccc66e41494e413a55e72332c4f255f76f34d71f
Дмитрий Губин <dgubin71@gmail.com>
2022-06-24 16:35:54 +0300

    Fix game_progression.py

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -12,17 +12,17 @@ def progression():
     while answer_ok != NUMBER_OF_ROUNDS:
         number1 = randrange(5)
         number2 = randrange(20)
-        double_point_pos = randrange(1, 11)
+        double_dot_pos = randrange(1, 11)
         question = ''
         for i in range(1, 11):
-            if i == double_point_pos:
+            if i == double_dot_pos:
                 true_answer = str((i + number1) * number2)
                 next_item = '..'
             else:
                 next_item = str((i + number1) * number2)
             question = f'{question} {next_item}'
 
-        if chek_answer(question, str(true_answer), name):
+        if chek_answer(question.strip(), str(true_answer), name):
             answer_ok += 1
         else:
             break
